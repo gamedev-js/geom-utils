cf5d4c10bdfb3c8f2cb63167800f575d6dd6eaba
shay_chen <641278529@qq.com>
2018-01-16 13:52:13 +0800

    Add ray_sphere and ray_box
    1. Add sphere.js and box.js
    2. Add ray_sphere and ray_box in intersect.js 
    3.Test ray_sphere and ray_box

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ export { default as line } from './lib/line';
 export { default as plane } from './lib/plane';
 export { default as ray } from './lib/ray';
 export { default as triangle } from './lib/triangle';
+export { default as sphere } from './lib/sphere';
+export { default as box } from './lib/box';
--- test/intersect.spec.js
+++ test/intersect.spec.js
@@ -1,6 +1,6 @@
 const tap = require('./tap');
 const { vec3 } = require('vmath');
-const { intersect, ray, line, triangle } = require('../dist/geom-utils');
+const { intersect, ray, line, triangle, sphere, box } = require('../dist/geom-utils');
 
 tap.test('intersect', t => {
 
@@ -10,15 +10,15 @@ tap.test('intersect', t => {
       0, 0.0, -1
     );
     let t1 = triangle.new(
-      0,    1, 0,
-     -0.5, -1, 0,
+      0, 1, 0,
+      -0.5, -1, 0,
       0.5, -1, 0
     );
     let out = vec3.create();
     let intersects = intersect.ray_triangle(r1, t1, out);
 
     t.assert(intersects);
-    t.equal_v3(out , [0, 0.5, 0]);
+    t.equal_v3(out, [0, 0.5, 0]);
 
     t.end();
   });
@@ -33,19 +33,19 @@ tap.test('intersect', t => {
       0, -0.2, -1
     );
     let t1 = triangle.new(
-      0,    1, 0,
-     -0.5, -1, 0,
+      0, 1, 0,
+      -0.5, -1, 0,
       0.5, -1, 0
     );
     let out = vec3.create();
     let intersects = intersect.line_triangle(l1, t1, out);
 
     t.assert(intersects);
-    t.equal_v3(out , [0, 0.5, 0]);
+    t.equal_v3(out, [0, 0.5, 0]);
 
     intersects = intersect.line_triangle(l2, t1, out);
     t.assert(intersects);
-    t.equal_v3(out , [0, 0.0, 0]);
+    t.equal_v3(out, [0, 0.0, 0]);
 
     t.end();
   });
@@ -63,7 +63,60 @@ tap.test('intersect', t => {
     );
 
     t.assert(intersects);
-    t.equal_v3(out , [0, 0.5, 0]);
+    t.equal_v3(out, [0, 0.5, 0]);
+
+    t.end();
+  });
+
+  t.test('ray_sphere', t => {
+    let r1 = ray.new(
+      0, 0, 0,
+      1, 1, 1
+    );
+    let s1 = sphere.new(2, 2, 2, 1);
+    let out = vec3.create();
+    let intersects = intersect.ray_sphere(r1, s1, out);
+
+    t.assert(intersects);
+    t.equal_v3(out, [2 - Math.sqrt(3) / 3, 2 - Math.sqrt(3) / 3, 2 - Math.sqrt(3) / 3]);
+
+    t.end();
+  });
+
+  t.test('ray_box', t => {
+    let r1 = ray.new(
+      0, 0, 0,
+      1, 0, 0
+    );
+    let axis_x = vec3.create();
+    let axis_y = vec3.create();
+    let axis_z = vec3.create();
+    vec3.set(axis_x, 1, 0, 0);
+    vec3.set(axis_y, 0, 1, 0);
+    vec3.set(axis_z, 0, 0, 1);
+    let box_point = vec3.create();
+    vec3.set(box_point, 2, 1, 0);
+    let ori_x = vec3.create();
+    let ori_y = vec3.create();
+    let ori_z = vec3.create();
+    let ori_point = vec3.create();
+    let angle = (-45) * Math.PI / 180;
+    let ori_rot = vec3.create();
+    vec3.rotateZ(ori_x, axis_x, ori_rot, angle);
+    vec3.rotateZ(ori_y, axis_y, ori_rot, angle);
+    vec3.rotateZ(ori_z, axis_z, ori_rot, angle);
+    vec3.rotateZ(ori_point, box_point, ori_rot, angle);
+    let b1 = box.new(
+      ori_point.x, ori_point.y, ori_point.z,
+      1, 1, 1,
+      ori_x.x, ori_x.y, ori_x.z,
+      ori_y.x, ori_y.y, ori_y.z,
+      ori_z.x, ori_z.y, ori_z.z,
+    );
+    let out = vec3.create();
+    let intersects = intersect.ray_box(r1, b1, out);
+    t.assert(intersects);
+    t.equal_v3(out, [Math.sqrt(2), 0, 0]);
 
     t.end();
   });
